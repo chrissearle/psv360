45420e283dac6ed7c264a0f43858c51da0d8bd4d
Chris Searle <chris@chrissearle.org>
2026-06-22 09:52:10 +0200

    Add prometheus

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  modules: ['@nuxt/ui', '@nuxt/eslint'],
+  modules: ['@nuxt/ui', '@nuxt/eslint', '@artmizu/nuxt-prometheus'],
 
   css: ['~/assets/css/main.css'],
 
